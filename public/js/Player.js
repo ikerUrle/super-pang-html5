789c8d245ec30d766ae803136ea515b5a8b7c1c7
ikerUrle <ikerurlezaga@gmail.com>
2020-03-31 20:18:25 +0200

    Volteando imÃagenes

--- public/js/Player.js
+++ public/js/Player.js
@@ -1,90 +1,92 @@
-import {Object2D, Vec2D} from "./math.js";
+import { Object2D, Vec2D } from "./math.js";
 import Settings from "./Settings.js";
 
-const frames = ['buster', 'buster-1','buster-2','buster-3'];
+const frames = ["buster", "buster-1", "buster-2", "buster-3"];
 
 export default class Player extends Object2D {
-
-    routeFrame(){
-        if(this.direction.x !== 0){
-            const frameIndex = Math.floor(this.distance / 10) % frames.length;
-            const frameName = frames[frameIndex];
-            return frameName;
-        }
-
-        return 'idle';
+  routeFrame() {
+    if (this.direction.x !== 0) {
+      const frameIndex = Math.floor(this.distance / 10) % frames.length;
+      const frameName = frames[frameIndex];
+      return frameName;
     }
 
-    constructor(size, pos, spriteSheet) {
-        super(size, pos);
-        this.force = new Vec2D(0, 0);
-        this.spriteSheet = spriteSheet;
-        this.direction = new Vec2D(0,0);
-        this.distance = 0;
+    return "idle";
+  }
+
+  constructor(size, pos, spriteSheet) {
+    super(size, pos);
+    this.force = new Vec2D(0, 0);
+    this.spriteSheet = spriteSheet;
+    this.direction = new Vec2D(0, 0);
+    this.distance = 0;
+  }
+
+  setHookManager(hookManager) {
+    this.hookManager = hookManager;
+  }
+
+  shoot() {
+    this.hookManager(this.position.x, this.position.y);
+  }
+
+  // time respresenta el tiempo que ha pasado desde la última ejecución
+  update(time) {
+    if (this.direction.x !== 0) {
+      this.distance += Settings.PLAYER_SPEED * time;
+    } else {
+      this.distance = 0;
     }
 
-    setHookManager(hookManager){
-        this.hookManager = hookManager;
-    }
-
-    shoot(){
-        this.hookManager(this.position.x,this.position.y);
-    }
-
-    // time respresenta el tiempo que ha pasado desde la última ejecución
-    update(time) {
-
-        if(this.direction.x !== 0){
-            this.distance += Settings.PLAYER_SPEED * time;
-        } else{
-            this.distance = 0;
-        }
-
-        /*
+    /*
         Asume por el momento que Settings.SCREEN_HEIGHT y Settings.SCREEN_WIDTH indican el tamaño de
         la pantalla del juego. Settings tiene otras constantes definidas (échales un vistazo)
         El objeto player tiene una altura (height) y una anchura (width) 
          */
 
-
-        if (this.y < Settings.SCREEN_HEIGHT-this.height-Settings.MARGIN){
-            this.force = this.force.add(Settings.GRAVITY * time); 
-            this.position = this.position.add(new Vec2D(0,this.force.y * time));
-            
-        }
-        
-        this.position = this.position.add(new Vec2D(this.direction.x * time * Settings.PLAYER_SPEED,0));
-       
-        // si buster está cayendo (está por debajo de la altura de la pantalla) 
-        // fuerza = añadir fuerza vertical de gravedad * tiempo
-        // position = añadir fuerza * tiempo al eje y
-
-        // position = añadir dirección * tiempo * velocidad del jugador al eje x
-        if (this.x < Settings.MARGIN){
-            this.position = new Vec2D(Settings.MARGIN,this.y);
-        } else if(this.x > Settings.SCREEN_WIDTH - this.width - Settings.MARGIN){
-            this.position = new Vec2D(Settings.SCREEN_WIDTH - this.width -Settings.MARGIN, this.y);
-        } 
-      
-
-
-        // si buster se sale por la izquierda de la pantalla
-        // position = 0,y
-      
-        // sino, si buster se sale por la derecha
-        // position =  lo más a la derecha sin salirse , y    
-      
-        if(this.y > Settings.SCREEN_HEIGHT){
-            this.position = new Vec2D(this.x,Settings.SCREEN_HEIGHT);
-        }
-
-        // si buster se sale por la parte inferior de la pantalla
-        // position = x, lo más abajo sin salirse
-      
+    if (this.y < Settings.SCREEN_HEIGHT - this.height - Settings.MARGIN) {
+      this.force = this.force.add(Settings.GRAVITY * time);
+      this.position = this.position.add(new Vec2D(0, this.force.y * time));
     }
 
-    draw(context) {
-        context.drawImage(this.spriteSheet.get(this.routeFrame()),this.x,this.y);
-        // pintar this.sprite en el contexto (en posicion x,y)
+    this.position = this.position.add(
+      new Vec2D(this.direction.x * time * Settings.PLAYER_SPEED, 0)
+    );
+
+    // si buster está cayendo (está por debajo de la altura de la pantalla)
+    // fuerza = añadir fuerza vertical de gravedad * tiempo
+    // position = añadir fuerza * tiempo al eje y
+
+    // position = añadir dirección * tiempo * velocidad del jugador al eje x
+    if (this.x < Settings.MARGIN) {
+      this.position = new Vec2D(Settings.MARGIN, this.y);
+    } else if (this.x > Settings.SCREEN_WIDTH - this.width - Settings.MARGIN) {
+      this.position = new Vec2D(
+        Settings.SCREEN_WIDTH - this.width - Settings.MARGIN,
+        this.y
+      );
     }
+
+    // si buster se sale por la izquierda de la pantalla
+    // position = 0,y
+
+    // sino, si buster se sale por la derecha
+    // position =  lo más a la derecha sin salirse , y
+
+    if (this.y > Settings.SCREEN_HEIGHT) {
+      this.position = new Vec2D(this.x, Settings.SCREEN_HEIGHT);
+    }
+
+    // si buster se sale por la parte inferior de la pantalla
+    // position = x, lo más abajo sin salirse
+  }
+
+  draw(context) {
+    context.drawImage(
+      this.spriteSheet.get(this.routeFrame(), this.direction.x),
+      this.x,
+      this.y
+    );
+    // pintar this.sprite en el contexto (en posicion x,y)
+  }
 }
--- public/js/SpriteSheet.js
+++ public/js/SpriteSheet.js
@@ -1,27 +1,43 @@
 export default class SpriteSheet {
+  constructor(image, width, height) {
+    this.image = image;
+    this.width = width;
+    this.height = height;
+    this.buffers = new Map();
+  }
 
-    constructor(image, width, height) {
-        this.image = image;
-        this.width = width;
-        this.height = height;
-        this.buffers = new Map();
-    }
+  define(name, x, y) {
+    const buffers = [false, true].map(flip => {
+      const buffer = document.createElement("canvas");
+      buffer.width = this.width;
+      buffer.height = this.height;
+      const context = buffer.getContext("2d");
+      if (flip) {
+        context.scale(-1, 1);
+        context.translate(-buffer.width, 0);
+      }
+      context.drawImage(
+        this.image,
+        x * this.width,
+        y * this.height,
+        this.width,
+        this.height,
+        0,
+        0,
+        this.width,
+        this.height
+      );
+      return buffer;
+    });
+    this.buffers.set(name, buffers);
+  }
 
-    define(name, x, y) {
-        const buffer = document.createElement('canvas');
-        buffer.width = this.width;
-        buffer.height = this.height;
-        buffer.getContext("2d").drawImage(this.image, x*this.width, y*this.height, this.width, this.height, 0, 0, this.width, this.height);
-        this.buffers.set(name, buffer);
-    }
+  draw(name, context, x, y) {
+    const buffer = this.buffers.get(name);
+    context.drawImage(buffer, x, y);
+  }
 
-    draw(name, context, x, y) {
-        const buffer = this.buffers.get(name);
-        context.drawImage(buffer, x, y);
-    }
-
-    get(name){
-        return this.buffers.get(name);
-    }
-
+  get(name, heading = 0) {
+    return this.buffers.get(name)[heading > 0 ? 1 : 0];
+  }
 }
