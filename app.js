f388e0a69d49d932212bda0a9de0810912cf11bb
ikerUrle <ikerurlezaga@gmail.com>
2020-09-03 17:51:15 +0200

    Dependencies removed

--- app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 5000;
-app.use(express.static('public'))
-
-app.listen(PORT,() => {
-   console.log(`Website serving on port ${PORT}...`);
-});
